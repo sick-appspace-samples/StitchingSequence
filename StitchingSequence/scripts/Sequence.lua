b6519a536b97a605ec379cbc68366c2680773c8b
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 13:04:27 +0100

    'Update'

--- StitchingSequence/scripts/Sequence.lua
+++ StitchingSequence/scripts/Sequence.lua
@@ -5,7 +5,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 1000 -- ms between visualization steps for demonstration purpose
 
 -- Create a viewer
-local viewer = View.create()
+local viewer = View.create('viewer2D1')
 
 --End of Global Scope-----------------------------------------------------------
 
